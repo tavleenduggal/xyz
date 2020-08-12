3f128d6e90fdfcc1e38e6536f68ede3639116d94
tavleenduggal <tavleenduggal@gmail.com>
2020-08-12 18:13:17 +0530

    Box image added

--- Box.js
+++ Box.js
@@ -8,6 +8,8 @@ class Box {
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
+
+    this.image=loadImage("sprites/wood1.png")
     
     World.add(world, this.body);
   }
@@ -17,11 +19,11 @@ class Box {
     push();
     translate(pos.x, pos.y);
     rotate(angle);
-    rectMode(CENTER);
+    imageMode(CENTER);
     strokeWeight(4);
     stroke("green");
     fill(255);
-    rect(0, 0, this.width, this.height);
+    image(this.image,0, 0, this.width, this.height);
     pop();
   }
 };
